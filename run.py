918217888cf0a384c44111faf7b24aeed5327253
Dan Uluma <danuluma@gmail.com>
2018-10-29 16:53:48 +0300

    [Feature #161543177] implemented add comment feature

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 users_list = [
     {       
             "userid":0,
@@ -7,9 +7,17 @@ users_list = [
             "role":'admin'
     }
 ]
-comments = []
+comments = [
+    {
+
+    }
+]
+user = []
 
 class Auth():
+    def __init__(self):
+        pass
+        
     def signup(self):
         """user register
                 
@@ -34,7 +42,8 @@ class Auth():
         self.confirm = confirm
         if self.password != self.confirm:
             print('Passwords should match')
-            return
+            print("PLease try agAIN")
+            return self.signup()
         self.role = 'user'
         user_dict = {
             "userid": len(users_list) + 1,
@@ -44,6 +53,67 @@ class Auth():
         }
         
         users_list.append(user_dict)
-        print("Successsfully registered in")
-        return 'Successfully registered'
-  
+        print("Successsfully registered in") 
+        return True   
+    
+    def login(self):
+        """user login 
+        
+        Arguments:
+            username {nic} -- [unique username]
+            password {nicki} -- [secret key to account]
+        
+        Returns:
+            success message
+        """
+        
+        print('Enter your username')
+        username=   input()
+        self.username = username
+        print('Enter your password')
+        password = input()
+        self.password = password
+        self.timestamp = datetime.now()
+        passw = [passw for passw in users_list if passw['password'] == self.password 
+                                          and passw['username'] == self.username]
+        if not passw:
+            print( 'Error logging in, check your credentials, try again')
+            return self.login()
+        self.logged_in_status = True
+        user.append(self.username)
+        print("logged in at {}".format(self.timestamp))
+        return True
+        
+
+    def logout(self):
+        if self.logged_in_status:
+            self.logged_in_status = False
+            return 'Successfully logged out' 
+        print("Please log in first")
+
+        return  self.login()
+        return True
+
+
+class Comment():
+    def create_comment(self):
+        self.added_by = user
+        print("Write your comment")
+        comment = input()
+        self.comment = comment
+        print("Your comment is {} and your name is {}".format(self.comment, user[0]))
+
+
+a = Auth()
+if a.signup():
+    print("Now log in")
+if a.login():
+    print("Dear user what do you want do?")
+    print("Enter c to comment or l to logout")
+    resp = input()
+    if resp == 'c':
+        b = Comment()
+        b.create_comment()
+    if resp == 'l':
+        a.logout()
+print("Sorry, invalid input,,quitting now")
